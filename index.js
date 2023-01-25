d2df17001fecc87960c59403afc47989c21ff87e
Gaba Bouliva <eaglebald111@gmail.com>
2023-01-25 04:23:07 +0100

    Add all arithmetic functionalities and connect to ui

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-/**
- * Add click events on all operands and operations
- * when clicked display on operations display
- * when user click on = sign  determine the operation he intends to do
- * call teh approriote arithmetic operation and retrn the result
- * update the result diplay div with the result
- */
+
+function operate(firstValue, operation, secondValue){
+  
+  /**
+   * Call the right arithmetic operation based on the operation sign
+   * return a String version of the result of the arithmethic operation
+   */
+
+  let firstNum = parseFloat(firstValue);
+  let secondNum = parseFloat(secondValue)
+
+  if(operation === '+'){
+    return (add(firstNum, secondNum)).toFixed(2);
+  }
+  if(operation === '-'){
+    return (subtract(firstNum, secondNum)).toFixed(2);
+  }
+  if(operation === '/'){
+    return (divide(firstNum, secondNum)).toFixed(2);
+  }
+  if(operation === 'x'){
+    return (multiply(firstNum, secondNum)).toFixed(2);
+  }
+}
+
+
  const add = function(firstNum, secondNum) {
 	return firstNum + secondNum;
 };
@@ -13,22 +32,29 @@ const subtract = function(firstNum, secondNum) {
 	return firstNum - secondNum;
 };
 
-const multiply = function(numbers) { 
+const multiply = function(firstNum, secondNum) { 
   
-  let total = numbers[0];
-  for (let i = 0; i < numbers.length - 1; i++) {
-    total  *= numbers[i+1];
-  }
-  return total;
+  return firstNum * secondNum;
 
 };
 
+const divide = function (firstNum, secondNum){
+  if (secondNum === '0') return 'undefined';
+  else return firstNum / secondNum;
+}
+
+let firstValue = '';
+let secondValue = '';
+let operation = '';
 
 
 const resultDisplaySpan = document.querySelector('.result-display>span');
 const inputsDisplaySpan = document.querySelector('.inputs-display>span');
 
 document.querySelector('.clear-operation').addEventListener('click', () =>{
+  firstValue = '';
+  secondValue = '';
+  operation = '';
   inputsDisplaySpan.textContent = '';
 })
 
@@ -36,17 +62,48 @@ document.querySelector('.clear-result').addEventListener('click', () =>{
   resultDisplaySpan.textContent = '';
 })
 
-// add click event on all operands
+/**
+ * Add click event on all operands
+ */
+
 document.querySelectorAll('.operand').forEach( (operandBtn) =>{
   operandBtn.addEventListener('click', () =>{
-    document.querySelector('.inputs-display>span').textContent += operandBtn.textContent; 
+
+    if(operandBtn.textContent !== '='){
+      document.querySelector('.inputs-display>span').textContent += operandBtn.textContent;
+    }
+
+    if(operandBtn.textContent === '='){
+      if(firstValue && secondValue){
+        let finalResult = operate(firstValue, operation, secondValue);
+        console.log(`Final Result = ${finalResult}`);
+        resultDisplaySpan.textContent = finalResult;
+      }
+    }
+    if(operation){
+      secondValue += operandBtn.textContent;
+    }else{
+      firstValue += operandBtn.textContent;
+    }
   })
 })
 
-// add click event on all operations
+/**
+ * Add click event listener on all arithmetic operation buttons
+ */
 document.querySelectorAll('.operation').forEach((mathOperationBtn) =>{
   mathOperationBtn.addEventListener('click', () =>{
     document.querySelector('.inputs-display>span').textContent += mathOperationBtn.textContent;
+    
+    if(operation){
+      let result = operate(firstValue, operation, secondValue);
+      firstValue = result;
+      secondValue = '';
+      operation = mathOperationBtn.textContent;
+      
+    }else{
+      operation = mathOperationBtn.textContent;
+    }
   })
 })
 
